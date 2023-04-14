be7930eff99db9f7f014a8950a0cd4338b8d7d20
erwangrostoutou <noahdenis85@yahoo.com>
2023-04-14 11:03:17 +0200

    ok

---
+++ FIFA 23/settings/overrideAutodetect.lua
@@ -0,0 +1,16 @@
+FullscreenEnabled = 1	     	   		  	       	     	    
+ResolutionWidth = 1920 	 	  		   	 		     
+ResolutionHeight = 1080  	      	     	      	      	      	     
+OverallGraphicsQuality = 1 	    	     	 	    	   	      
+VSyncEnabled = 1    	  	      	  	   	     	   	   
+PresentInterval = 1       	    		       	  	   
+WindowedBorderless = 0	       	      	   	 	 	     	 
+StrandBasedHair =  0    	       	      	  	    		     
+UseGoalNets3d =  -1     	      	  	  	  	  	 
+DynamicResolution =  0      	       	    	 	   	       	   
+      	     	  	  	      	    	       	 		       
+    	  	     	    	  	    		       	    
+	       	   	    	     	  	 		      	   
+	      	   	 	   	 	     	 	    	   
+    	  	       	      	      	      	       		
+
